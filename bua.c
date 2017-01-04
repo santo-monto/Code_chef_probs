51a73f6a63638a47e67ce9d953a3aee2c97de69a
santo-monto <santoanand2@gmail.com>
2017-01-04 20:14:23 +0530

    Add files via upload

--- bua.c
+++ bua.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
@@ -17,22 +18,37 @@ int main()
   //  printf("Enter a number;");
     scanf("%d",&n);
 
-    int i=3;
+    int i=2;
     int a=n;
+    int b=sqrt(n);
+    int c=sqrt(b);
 
     if(n==1){ printf("INFINITY\n"); k++; continue;}
 
-    for(i=3;i<=n/2;i++){
+    for(i=2;i<=c;i++){
       mul = 1;
       while(mul*i<=a) mul*=i;
       if(a/mul==1)count++;
   }
-  if(n>1)
-  {
-    if(n%2!=0)printf("%d",count+2+(n/2));
-    else printf("%d",count+(n/2)+1);
+  //printf("%d\n",count);
+
+  for(i=c+1;i<=b;i++)
+  { int p1=pow(i,3);
+    int p2=pow(i,2);
+    if(n/p1==1) count++;
+    else if(n/p2==1) count++;
+
   }
-  else printf("%d",count);
+  //printf("%d\n",count);
+
+  if(i>b)
+  {
+    if(n==3) printf("%d\n",2);
+    else if(n==2) printf("%d\n",1);
+    else if(n%2!=0)printf("%d\n",count+1+(n/2));
+    else printf("%d\n",count+(n/2));
+  }
+  else printf("%d\n",count);
   k++;
   }
 
